6652592fc96e6ea0002f2de0cf7d159e2c931e74
Kea01 <keabetswemabeba@gmail.com>
2023-03-01 00:01:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+window.onload = function (){
+
+    document.getElementById("title").innerHTML = "Frontend Development";
+    
+    document.getElementById("features").innerHTML = "Features";
+
+    document.getElementById("company").innerHTML = "Company";
+
+    document.getElementById("history").innerHTML = "History";
+    document.getElementById("team").innerHTML = "Our Team";
+    document.getElementById("blog").innerHTML = "Blog";
+
+    document.getElementById("careers").innerHTML = "Careers";
+    document.getElementById("about").innerHTML = "About";
+
+    document.getElementById("login").innerHTML = "Login";
+    document.getElementById("register").innerHTML = "Register";
+
+    document.getElementById("h1").innerHTML = "Make <br>Remote Work";
+    document.getElementById("ptag").innerHTML = "Get your team in sync, no matter your location.<br> Streamline processes, create team rituals,<br> and watch productivity soar.";
+    document.getElementById("lmore").innerHTML = "Learn More";
+}
